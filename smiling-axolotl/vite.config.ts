682394c2eb08f24890652f974526461633949ac6
David <dvdalelo@ciencias.unam.mx>
2025-08-31 03:21:20 -0600

    Added deploy branch

--- smiling-axolotl/vite.config.ts
+++ smiling-axolotl/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: '/', // Since this is a .github.io repo, deploy to root
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
