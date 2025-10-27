92a0073bda146ff3379456d865f26637a1981143
Samuel <69873276+Vexot@users.noreply.github.com>
2025-10-27 18:29:27 -0500

    slight change

--- smiling-axolotl/src/components/ContactModal.vue
+++ smiling-axolotl/src/components/ContactModal.vue
@@ -304,7 +304,7 @@ export default {
 
 .form-group input::placeholder,
 .form-group textarea::placeholder {
-  color: rgba(255, 255, 255, 0.6);
+  color: rgba(255, 255, 255, 0.7);
 }
 
 .form-group input:focus,
